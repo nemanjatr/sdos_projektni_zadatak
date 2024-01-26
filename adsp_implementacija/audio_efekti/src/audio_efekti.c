1bc6a0da17e3d260173c1ea700d7ce8e76e5c4e3
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-01-26 23:26:42 +0100

    -Izmjene u Python implementaciji koje su propuštene u prethodnom commit-u
      - Dodavanja ćelija za ispis svakog rezultata primjene efekta u odgovarajući fajl u folderu python_implementacija/rezultati
    
      -  Uklanjanje nekih ćelija koje su trenutno nepotrebne
    
      - Dodavanje markdown ispisa radi preglednosti
    
    -Izmjene u ADSP implementaciji
    
      - Dodavanje implementacije efekta distorzije
    
      - Pocetak rada na wah wah efektu
    
    - Dodavanje osnovnih informacija o efektu distorzije u izvještaj

--- adsp_implementacija/audio_efekti/src/audio_efekti.c
+++ adsp_implementacija/audio_efekti/src/audio_efekti.c
@@ -4,24 +4,27 @@
 
 #include <sys/platform.h>
 #include "adi_initialize.h"
-#include "audio_efekti.h"
+#include "test.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <filter.h>
+#include <math.h>
 #include "test_audio.h"
+
 
 // Index for memory allocation
 int index = 0;
 
 // Reserve 150 of SDRAM memory for signals
 #pragma section("seg_sdram")
-static char heap_mem[512000];
+static char heap_mem[1000000];
 
 /** 
  * If you want to use command program arguments, then place them in the following string. 
  */
 char __argv_string[] = "";
 
-
+/* Funkcija koja vrsi obradu ulaznog signala u skladu sa audio efektom kasnjenja (delay) */
 void delay(float *x, float *y, int M, float g)
 {
 	printf("U funkciji delay\n");
@@ -46,18 +49,135 @@ void delay(float *x, float *y, int M, float g)
 	heap_free(index, x_delay);
 }
 
+/* Funkcija koja vrsi obradu u skladu sa audio efektom distorzije (distortion) */
+void distortion(float* x, float* y, float gain, float mix)
+{
+	float* x_distort = (float* )heap_calloc(index, LEN, sizeof(float));
+	float* x_gain = (float* )heap_calloc(index, LEN, sizeof(float));
+
+	float max = 0.0;
+
+	printf("Prije prve for petlje\n");
+	for(int i = 0; i < LEN; i++)
+	{
+		if(fabsf(x[i]) > max)
+		{
+			max = fabsf(x[i]);
+		}
+	}
+
+	printf("Prije druge for petlje\n");
+	for(int i = 0; i < LEN; i++)
+	{
+		x[i] = x[i] / max;
+	}
+
+	printf("Prije 2 i po petlje\n");
+	for(int i = 0; i < LEN; i++)
+	{
+		x_gain[i] = gain * x[i];
+	}
+
+	printf("Prije trece for petlje\n");
+	for(int i = 0; i < LEN; i++)
+	{
+		float x_original = x[i];
+		float x_value = x_gain[i];
+		float abs_x = fabsf(x_value);
+		float exp_x = expf(-1 * ((x_value * x_value) / abs_x));
+
+		x_distort[i] = (x_value / abs_x) *(1 - exp_x);
+		y[i] = (1 - mix) * x_original + mix * x_distort[i];
+
+	}
+
+
+	printf("Prije cetvrte for petlje\n");
+	max = 0.0;
+	for(int i = 0; i < LEN; i++)
+	{
+		if(fabsf(y[i]) > max)
+		{
+			max = fabsf(y[i]);
+		}
+	}
+
+	printf("Prije pete for petlje\n");
+	for(int i = 0; i < LEN; i++)
+	{
+		y[i] = y[i] / max;
+	}
+
+	heap_free(index, x_distort);
+}
+
+
+void wah_wah(float* x, float* y)
+{
+	float minf = 500.0;
+	float maxf = 3000.0;
+	float fw = 2000.0;
+	float delta = fw / FS;
+	float* freq_carrier = NULL;
+	freq_carrier = (float* )heap_malloc(index, LEN*sizeof(float));
+	if(freq_carrier == NULL)
+	{
+		printf("Error\n");
+		return;
+	}
+
+	printf("Prvi\n");
+
+	int no_iterations = (int)((maxf - minf) / delta) + 1;
+	int i = 1;
+
+	freq_carrier[0] = minf;
+	for(i = 1; i < no_iterations; i++)
+	{
+		freq_carrier[i] = freq_carrier[i - 1] + delta;
+	}
+
+	printf("Drugi\n");
+	while(i < LEN)
+	{
+		for(int j = (no_iterations - 1); j >= 0; j--)
+		{
+			freq_carrier[i] = freq_carrier[j];
+			i++;
+		}
+
+		for(int j = 0; j < no_iterations - 1; j++)
+		{
+			freq_carrier[i] = freq_carrier[j];
+			i++;
+		}
+	}
+
+	printf("Treci\n");
+	FILE *fp = fopen("../output_files/trougaoni.txt", "w");
+	if(fp == NULL)
+	{
+		printf("File open failed/n");
+		return;
+	}
+	printf("Otvoren fajl\n");
+	for(int i = 0; i < LEN; i++)
+	{
+		fprintf(fp, "%f\n", freq_carrier[i]);
+	}
+	fclose(fp);
+	printf("Zatvoren fajl\n");
+
+
+
+	heap_free(index, freq_carrier);
+}
 
 
 int main(int argc, char *argv[])
 {
-	/**
-	 * Initialize managed drivers and/or services that have been added to 
-	 * the project.
-	 * @return zero on success 
-	 */
+
 	adi_initComponents();
-	
-	printf("%d", sizeof(float));
 
 	int uid = 999;  /* arbitrary userid for heap */
 	float *audio_input = 0;
@@ -95,31 +215,42 @@ int main(int argc, char *argv[])
 		audio_input[i] = test_signal[i];
 	}
 
-	printf("Prije delay funkcije\n");
+	wah_wah(audio_input, audio_output);
+
 
 	/* Delay */
-	delay(audio_input, audio_output, 1102, 0.5);
+	//delay(audio_input, audio_output, 1102, 0.5);
 
-	printf("Nakon delay funkcije\n");
+	//distortion(audio_input, audio_output, 20, 0.8);
 
 
-
-	FILE *fp = fopen("../output_files/delay.txt", "w");
-	if(fp == NULL)
-	{
-		printf("File open failed/n");
-		return -1;
-	}
-	printf("Otvoren fajl\n");
-	for(int i = 0; i < LEN; i++)
-	{
-		fprintf(fp, "%f\n", audio_output[i]);
-	}
-	fclose(fp);
-	printf("Zatvoren fajl\n");
+	/* Ispis rezultata obrade u fajl */
+//	FILE *fp = fopen("../output_files/*.txt", "w");
+//	if(fp == NULL)
+//	{
+//		printf("File open failed/n");
+//		return -1;
+//	}
+//	printf("Otvoren fajl\n");
+//	for(int i = 0; i < LEN; i++)
+//	{
+//		fprintf(fp, "%f\n", audio_output[i]);
+//	}
+//	fclose(fp);
+//	printf("Zatvoren fajl\n");
 
 	heap_free(index, audio_input);
 	heap_free(index, audio_output);
+
+
+	/* Print it out */
+//	for(int i = 0; i < LEN; i++){
+//		printf("%.8f, ", audio_input[i]);
+//		if((i+1) % 5 == 0){
+//			printf("\n");
+//		}
+//	}
+
 
 	return 0;
 }
