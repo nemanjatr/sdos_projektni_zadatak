6712792088aff7ef8ee7e1336a9f550ece638b98
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-01-28 10:20:54 +0100

    Izmjena izvještaja:
      -Dodavanje teorijskog uvoda o ostalim efektima. Kao i kratkih pojašnjenja implementacije u python-u svih efekata
    
    Izmjena adsp implementacije:
    
      - Dodavanje kompletne implementacije wah-wah efekta, kao i koda za efekte phaser i reverb
    
      - Dodavanje .h fajla sa filtrima potrebnim za wah-wah i phaser efekat

--- adsp_implementacija/audio_efekti/src/audio_efekti.c
+++ adsp_implementacija/audio_efekti/src/audio_efekti.c
@@ -10,6 +10,10 @@
 #include <filter.h>
 #include <math.h>
 #include "test_audio.h"
+#include "iir_peak_a.h"
+#include "iir_peak_b.h"
+#include "iir_notch_a.h"
+#include "iir_notch_b.h"
 
 
 // Index for memory allocation
@@ -24,15 +28,16 @@ static char heap_mem[1000000];
  */
 char __argv_string[] = "";
 
+
 /* Funkcija koja vrsi obradu ulaznog signala u skladu sa audio efektom kasnjenja (delay) */
 void delay(float *x, float *y, int M, float g)
 {
-	printf("U funkciji delay\n");
 	float *x_delay = (float *)heap_calloc(index, LEN, sizeof(float));
 	int k = 0;
 
-	printf("Prije prve for petlje\n");
-	for(int i = 0; i < LEN; i++)
+	int i;
+
+	for(i = 0; i < LEN; i++)
 	{
 		if(i > M - 1)
 		{
@@ -41,8 +46,8 @@ void delay(float *x, float *y, int M, float g)
 		}
 	}
 
-	printf("Prije druge for petlje\n");
-	for(int i = 0; i < LEN; i++)
+
+	for(i = 0; i < LEN; i++)
 	{
 		y[i] += x[i] + g * x_delay[i];
 	}
@@ -111,9 +116,10 @@ void distortion(float* x, float* y, float gain, float mix)
 	heap_free(index, x_distort);
 }
 
-
+/* Funkcija koja vrsi obradu u skladu sa audio efektom wah-wah */
 void wah_wah(float* x, float* y)
 {
+	/* Prvi dio algoritma. Kreiranje trougaonog signala nosioca frekvencija */
 	float minf = 500.0;
 	float maxf = 3000.0;
 	float fw = 2000.0;
@@ -154,23 +160,136 @@ void wah_wah(float* x, float* y)
 	}
 
 	printf("Treci\n");
-	FILE *fp = fopen("../output_files/trougaoni.txt", "w");
-	if(fp == NULL)
-	{
-		printf("File open failed/n");
-		return;
-	}
-	printf("Otvoren fajl\n");
-	for(int i = 0; i < LEN; i++)
-	{
-		fprintf(fp, "%f\n", freq_carrier[i]);
-	}
-	fclose(fp);
-	printf("Zatvoren fajl\n");
 
+	/* Drugi dio algoritma. */
+	float a[3];
+	float b[3];
+	for(int i = 2; i < LEN; i++)
+	{
+		for(int j = 0; j < 3; j++){
+			a[j] = peak_a_coeff[(i-2)*3 + j];
+			b[j] = peak_b_coeff[(i-2)*3 + j];
+		}
+		y[i] = b[0]*x[i] + b[1]*x[i-1] + b[2]*x[i-2] - a[1]*y[i-1] - a[2]*y[i-2];
+	}
 
 
 	heap_free(index, freq_carrier);
+}
+
+/* Funkcija koja vrsi obradu skladu sa phaser efektom */
+void phaser(float* x, float* y)
+{
+	float minf = 500.0;
+	float maxf = 3000.0;
+	float fw = 2000.0;
+	float delta = fw / FS;
+
+	float* y_first = NULL;
+	y_first = (float* )heap_malloc(index, LEN*sizeof(float));
+	if(y_first == NULL)
+	{
+		printf("Error\n");
+		return;
+	}
+
+	float* freq_carrier = NULL;
+	freq_carrier = (float* )heap_malloc(index, LEN*sizeof(float));
+	if(freq_carrier == NULL)
+	{
+		printf("Error\n");
+		return;
+	}
+
+	printf("Prvi\n");
+
+	int no_iterations = (int)((maxf - minf) / delta) + 1;
+	int i = 1;
+
+	freq_carrier[0] = minf;
+	for(i = 1; i < no_iterations; i++)
+	{
+		freq_carrier[i] = freq_carrier[i - 1] + delta;
+	}
+
+	printf("Drugi\n");
+	while(i < LEN)
+	{
+		for(int j = (no_iterations - 1); j >= 0; j--)
+		{
+			freq_carrier[i] = freq_carrier[j];
+			i++;
+		}
+
+		for(int j = 0; j < no_iterations - 1; j++)
+		{
+			freq_carrier[i] = freq_carrier[j];
+			i++;
+		}
+	}
+
+	printf("Treci\n");
+
+	/* Drugi dio algoritma. */
+	float a[3];
+	float b[3];
+	for(int i = 2; i < LEN; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			a[j] = notch_a_coeff[(i-2)*3 + j];
+			b[j] = notch_b_coeff[(i-2)*3 + j];
+		}
+		y_first[i] = b[0]*x[i] + b[1]*x[i-1] + b[2]*x[i-2] - a[1]*y_first[i-1] - a[2]*y_first[i-2];
+	}
+
+
+	for(int i = 2; i < LEN; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			a[j] = notch_a_coeff[(i-2)*3 + j];
+			b[j] = notch_b_coeff[(i-2)*3 + j];
+		}
+		y[i] = b[0]*y_first[i] + b[1]*y_first[i-1] + b[2]*y_first[i-2] - a[1]*y[i-1] - a[2]*y[i-2];
+
+	}
+	heap_free(index, freq_carrier);
+
+}
+
+/* Funkcija koja vrsi obradu signal u skladu se efektom reverberacija */
+void reverb(float* x, float* y, float alpha, int N, int R)
+{
+	for(int i = 0; i < LEN; i++)
+	{
+		for(int k = 0; k < N; k++)
+		{
+			y[i] += pow(alpha, k) * ((i > k*R) ? x[i - k*R] : 0);
+		}
+	}
+
+
+	/* Normalizacija izlaznog niza
+	 * !! Mozda i nije potrebna
+	 */
+	float max = 0.0;
+	for(int i = 0; i < LEN; i++)
+	{
+		if(fabsf(y[i]) > max)
+		{
+			max = fabsf(y[i]);
+		}
+	}
+
+	printf("Prije pete for petlje\n");
+	for(int i = 0; i < LEN; i++)
+	{
+		y[i] = y[i] / max;
+	}
+
+
+
 }
 
 
@@ -215,17 +334,24 @@ int main(int argc, char *argv[])
 		audio_input[i] = test_signal[i];
 	}
 
-	wah_wah(audio_input, audio_output);
-
-
 	/* Delay */
 	//delay(audio_input, audio_output, 1102, 0.5);
 
+	/* Distortion */
 	//distortion(audio_input, audio_output, 20, 0.8);
 
+	/* Wah-Wah */
+	//wah_wah(audio_input, audio_output);
+
+
+	/* Phaser */
+	//phaser(audio_input, audio_output);
+
+	/* Reverb */
+	//reverb(audio_input, audio_output, 0.8, 10, 8);
 
 	/* Ispis rezultata obrade u fajl */
-//	FILE *fp = fopen("../output_files/*.txt", "w");
+//	FILE *fp = fopen("../output_files/delay_optimize.txt", "w");
 //	if(fp == NULL)
 //	{
 //		printf("File open failed/n");
@@ -238,18 +364,9 @@ int main(int argc, char *argv[])
 //	}
 //	fclose(fp);
 //	printf("Zatvoren fajl\n");
-
+//
 	heap_free(index, audio_input);
 	heap_free(index, audio_output);
-
-
-	/* Print it out */
-//	for(int i = 0; i < LEN; i++){
-//		printf("%.8f, ", audio_input[i]);
-//		if((i+1) % 5 == 0){
-//			printf("\n");
-//		}
-//	}
 
 
 	return 0;
